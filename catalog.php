44ab2a09292dfedab34c80074ff3a6d528518274
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-02-15 13:35:41 -0500

    dynamic catalog page

--- catalog.php
+++ catalog.php
@@ -1,10 +1,24 @@
 <?php 
-    $pageTitle = "Full Catalog";
+
+    switch($_GET["cat"]) {
+        case "books":
+            $pageTitle = "Books";
+            break;
+        case "movies":
+            $pageTitle = "Movies";
+            break;
+        case "music":
+            $pageTitle = "Music";
+            break;
+        default:
+            $pageTitle = "Full Catalog";
+    }
+
     include("./partials/header.php"); 
 ?>
 
 <div class="section page">
-    <h1>Full Catalog</h1>
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("./partials/footer.php"); ?>
