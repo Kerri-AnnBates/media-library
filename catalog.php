5c6cd19653d9a5ba2f2e316ef57e69ff3677f2ad
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-02-16 12:22:38 -0500

    display items

--- catalog.php
+++ catalog.php
@@ -1,4 +1,5 @@
 <?php
+    include("./includes/data.php");
 
     $pageTitle = "Full Catalog";
     $section = null;
@@ -25,16 +26,17 @@
     
 
     include("./includes/header.php");
-    include("./includes/data.php");
 ?>
 
 <div class="section catalog page">
     <div class="wrapper">
         <h1><?php echo $pageTitle; ?></h1>
-        <ul>
+        <ul class="items">
             <?php 
                 foreach ($catalog as $item) {
-                    echo "<li>" . $item["title"] . "</li>";
+                    echo "<li><a href='#'><img src='" . $item["img"] . "' alt='" . $item["title"] . "' />"
+                            . "<p>View Details</p>" .
+                        "</a></li>";
                 }
             ?>
         </ul>
